c3ba44b2cf5a6f2046c1d5622e03212957461375
Sebastian Huber <sebastian.huber@embedded-brains.de>
2018-11-14 07:41:50 +0100

    FIXME initialize system counter

--- bsps/arm/imx/start/bspstart.c
+++ bsps/arm/imx/start/bspstart.c
@@ -58,11 +58,54 @@ uint32_t bsp_fdt_map_intr(const uint32_t *intr, size_t icells)
   return intr[1] + MAGIC_IRQ_OFFSET;
 }
 
+/* System Counter */
+struct sctr_regs {
+	uint32_t cntcr;
+	uint32_t cntsr;
+	uint32_t cntcv1;
+	uint32_t cntcv2;
+	uint32_t resv1[4];
+	uint32_t cntfid0;
+	uint32_t cntfid1;
+	uint32_t cntfid2;
+	uint32_t resv2[1001];
+	uint32_t counterid[1];
+};
+
+#define SC_CNTCR_ENABLE		(1 << 0)
+#define SC_CNTCR_HDBG		(1 << 1)
+#define SC_CNTCR_FREQ0		(1 << 8)
+#define SC_CNTCR_FREQ1		(1 << 9)
+
+#define SCTR_BASE_ADDR              (0x21dC000)
+int timer_init(void)
+{
+	struct sctr_regs volatile *sctr = (struct sctr_regs *)SCTR_BASE_ADDR;
+	unsigned long val, freq;
+
+	freq = 8000000;
+	asm volatile("mcr p15, 0, %0, c14, c0, 0" : : "r" (freq));
+
+	sctr->cntfid0 = freq;
+
+	/* Enable system counter */
+	val = sctr->cntcr;
+	val &= ~(SC_CNTCR_FREQ0 | SC_CNTCR_FREQ1);
+	val |= SC_CNTCR_FREQ0 | SC_CNTCR_ENABLE | SC_CNTCR_HDBG;
+	sctr->cntcr = val;
+
+	return 0;
+}
+
 void arm_generic_timer_get_config(
   uint32_t *frequency,
   uint32_t *irq
 )
 {
+  timer_init();
+  *frequency = 8000000;
+  *irq = 29;
+  return;
   const void *fdt;
   int node;
   int len;
