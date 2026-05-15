7bfd39f149e6ed523804e50023cb4c5f4ee3a811
Sebastian Huber <sebastian.huber@embedded-brains.de>
2026-05-15 17:22:30 +0200

    too much
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

--- cpukit/include/rtems.h
+++ cpukit/include/rtems.h
@@ -5,7 +5,7 @@
  *
  * @ingroup RTEMSAPIClassic
  *
- * @brief This header file defines the RTEMS Classic API.  Oops.
+ * @brief This header file defines the RTEMS Classic API.  Oops. More.
  */
 
 /*
